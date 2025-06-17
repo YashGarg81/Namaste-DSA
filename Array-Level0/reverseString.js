398dac05a90ff4253bc500d364ddf8adc5cb6c60
Yash Garg <yash.garg.cse.2021@miet.ac.in>
2025-06-18 00:50:44 +0530

    Updated Reverse String

--- Array-Level0/reverseString.js
+++ Array-Level0/reverseString.js
@@ -1,12 +1,15 @@
 //Write a function that reverses a string. The input string is given as an array of characters s.
 
-var reverseString = function(s) {
-    let len = s.length;
-    let halfLen = Math.floor(len / 2);
+function reverseString() {
+  let arr = ["h", "e", "l", "l", "o"];
+  let loopCount = Math.floor(arr.length / 2);
+  for (let i = 0; i < loopCount; i++) {
+    // swap two value beetween pointer1 arr[i] to pointer2 arr[arr.length - 1 - i];
+    let temp = arr[i];
+    arr[i] = arr[arr.length - 1 - i];
+    arr[arr.length - 1 - i] = temp;
+  }
+  console.log(arr);
+}
 
-    for (let i = 0; i < halfLen; i++) {
-        let temp = s[i];
-        s[i] = s[len - i - 1];
-        s[len - i - 1] = temp;
-    }
-};
+reverseString();
