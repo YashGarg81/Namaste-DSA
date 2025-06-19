7727f4254d2422810bbe314bced6892581793889
Yash Garg <yash.garg.cse.2021@miet.ac.in>
2025-06-20 01:01:08 +0530

    MergeSortedArray

--- Array-Level0/MergeSortedarray.js
+++ Array-Level0/MergeSortedarray.js
@@ -33,6 +33,10 @@ console.log(nums1);  // ✅ Output: [1, 2, 2, 3, 5, 6]
 // Space Complexity
 
 //We create a copy of the first m elements of nums1 in nums1Copy, which takes O(m) additional space.
+
+// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.Merge nums1 and nums2 into a single array sorted in non-decreasing order.The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+
 // Overall: O(m)
 
 // Approach 2
